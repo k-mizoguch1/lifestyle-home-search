dc7b1a1da43a12fbc84bfdc898f1bf8ef9c6756e
Tatsu821 <tatsuhiro821@outlook.jp>
2025-03-08 16:45:51 +0900

    del route.ts

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -1,14 +1,14 @@
-import { parseCsv } from '@/lib/parseCsv'
-import { NextResponse } from 'next/server'
+// import { parseCsv } from '@/lib/parseCsv'
+// import { NextResponse } from 'next/server'
 
-export async function GET() {
-  try {
-    return NextResponse.json(await parseCsv(), { status: 200 })
-  } catch (error) {
-    console.error(error)
-    return NextResponse.json(
-      { error: 'Failed to read CSV file' },
-      { status: 500 },
-    )
-  }
-}
+// export async function GET() {
+//   try {
+//     return NextResponse.json(await parseCsv(), { status: 200 })
+//   } catch (error) {
+//     console.error(error)
+//     return NextResponse.json(
+//       { error: 'Failed to read CSV file' },
+//       { status: 500 },
+//     )
+//   }
+// }
