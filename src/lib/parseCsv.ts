77e0784ee8662e28fde4060375773eba93371254
Tatsu821 <tatsuhiro821@outlook.jp>
2025-03-08 16:43:55 +0900

    del parseCSV

--- src/lib/parseCsv.ts
+++ src/lib/parseCsv.ts
@@ -1,36 +1,36 @@
-import { Building, Home, Layout } from '@/model/home'
-import fs from 'fs/promises'
-import path from 'path'
+// import { Building, Home, Layout } from '@/model/home'
+// import fs from 'fs/promises'
+// import path from 'path'
 
-export async function parseCsv(): Promise<Home[]> {
-  const filePath = path.join(process.cwd(), 'public', 'data.csv')
+// export async function parseCsv(): Promise<Home[]> {
+//   const filePath = path.join(process.cwd(), 'public', 'data.csv')
 
-  try {
-    const fileContent = await fs.readFile(filePath, 'utf-8')
+//   try {
+//     const fileContent = await fs.readFile(filePath, 'utf-8')
 
-    const rows = fileContent.split('\n').map((row) => row.split(','))
-    const headers = rows.shift()
+//     const rows = fileContent.split('\n').map((row) => row.split(','))
+//     const headers = rows.shift()
 
-    if (!headers) {
-      return []
-    }
+//     if (!headers) {
+//       return []
+//     }
 
-    const jsonData: Home[] = rows.map((row) => {
-      return {
-        id: row[0],
-        name: row[1],
-        prefecture: row[2],
-        city: row[3],
-        rent: Number(row[4]) || 0,
-        layout: row[5] as Layout,
-        year: Number(row[6]) || 0,
-        building: row[7] as Building,
-      }
-    })
+//     const jsonData: Home[] = rows.map((row) => {
+//       return {
+//         id: row[0],
+//         name: row[1],
+//         prefecture: row[2],
+//         city: row[3],
+//         rent: Number(row[4]) || 0,
+//         layout: row[5] as Layout,
+//         year: Number(row[6]) || 0,
+//         building: row[7] as Building,
+//       }
+//     })
 
-    return jsonData
-  } catch (error) {
-    console.error('Error reading the CSV file:', error)
-    return []
-  }
-}
+//     return jsonData
+//   } catch (error) {
+//     console.error('Error reading the CSV file:', error)
+//     return []
+//   }
+// }
